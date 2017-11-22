efea250c2d9afcd4b04d411ae88fca32e1fb118b
Hatem Noureddine <hateuum@gmail.com>
2017-11-22 17:59:54 +0100

    create empty project

---
+++ app/src/main/java/com/ygl_consulting/myfluxrss/ListRssActivity.java
@@ -0,0 +1,13 @@
+package com.ygl_consulting.myfluxrss;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ListRssActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_rss);
+    }
+}
